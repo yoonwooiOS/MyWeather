ee696158e339d216ec278a80549849b2c3fad1a1
yoonwooiOS <yoonwoo4429@kakao.com>
2024-06-19 16:41:33 +0900

    ✨ feat : 폰트 Reasource 추가

--- MyWeather/Resource/Resource.swift
+++ MyWeather/Resource/Resource.swift
@@ -5,4 +5,18 @@
 //  Created by 김윤우 on 6/19/24.
 //
 
-import Foundation
+import UIKit
+
+enum Label {
+    
+    enum Font {
+        
+        static let regular12 = UIFont.systemFont(ofSize: 12)
+        static let regular14 = UIFont.systemFont(ofSize: 14)
+        static let regular16 = UIFont.systemFont(ofSize: 16)
+        
+        static let bold16 = UIFont.boldSystemFont(ofSize: 16)
+        
+    }
+    
+}
